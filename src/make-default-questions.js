0e64578a9c6ab758b92779dc49ef2c29d1c27c1f
Guilherme Monte <gmonte@inf.ufsm.br>
2025-05-23 04:34:15 -0300

    fixing spaces

--- src/make-default-questions.js
+++ src/make-default-questions.js
@@ -7,10 +7,10 @@ module.exports = (allPackages, changedPackages) => ([
     message: 'Select the type of change that you\'re committing:',
     choices: [
       {value: 'feat',     name: 'feat:     ✨  A new feature (note: this will indicate a release)'},
-      {value: 'fix',      name: 'fix:      🛠  A bug fix (note: this will indicate a release)'},
+      {value: 'fix',      name: 'fix:      🐛  A bug fix (note: this will indicate a release)'},
       {value: 'docs',     name: 'docs:     📝  Documentation only changes'},
       {value: 'style',    name: 'style:    🎨  Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)'},
-      {value: 'refactor', name: 'refactor: 🔨  A code change that neither fixes a bug nor adds a feature'},
+      {value: 'refactor', name: 'refactor: 🔨  A code change that neither fixes a bug nor adds a feature'}, 
       {value: 'perf',     name: 'perf:     🚀  A code change that improves performance'},
       {value: 'test',     name: 'test:     🧪  Adding missing tests'},
       {value: 'chore',    name: 'chore:    🔧  Changes to the build process or auxiliary tools\n            and libraries such as documentation generation'},
